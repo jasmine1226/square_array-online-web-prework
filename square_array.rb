e5155084543c8acd5854b33ff3cd16a974bb96aa
Jasmine Chen <jasmine1226@gmail.com>
2019-05-06 04:04:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   # your code here
   size = array.size
   count = 0
-  array.each |count|
-    array[count] = array[count] ** 2
+  array.each |num|
+    num = num ** 2
     count += 1
   end
 end
